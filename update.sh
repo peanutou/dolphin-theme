5c52190bd3bb74b28c682073d9592bb84e2680a7
peanutou <peanutou@msn.com>
2018-05-01 19:54:27 +0800

    --themes=dolphin-theme

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 sudo -H -u edxapp bash << EOF
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform
-paver update_assets lms --settings=aws --debug
+paver update_assets lms --themes=dolphin-theme --settings=aws --debug
 EOF
 
 # restart lms instance
